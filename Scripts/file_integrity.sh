62c217df4340e47caef9bf66129e4b4ad56415bf
kbroccof <kbroccof@uci.edu>
2020-11-09 08:45:26 -0800

    File Integrity for All Chromosomes

--- Scripts/file_integrity.sh
+++ Scripts/file_integrity.sh
@@ -1,9 +1,24 @@
 #Summarize Genome Assembly 
 ##File download and File Integrity on gzipped fast file using a checksum 
 
+#Download the file 
 all_Chromosomes="ftp://ftp.flybase.net/genomes/dmel/current/fasta/dmel-all-chromosome-r6.36.fasta.gz"\
 wget $all_Chromosomes
 less dmel-all-chromosome-r6.36.fasta.gz
 q
+
+#Name the file 
+all_chromosomes="dmel-all-chromosome-r6.36.fasta.gz"
+less $all_chromosomes
+q
+
+
+#Checksum
+md5sum $all_chromosomes
+#output: fbd2855a20c3610050ff2496dd975821  dmel-all-chromosome-r6.36.fasta.gz 
+#md5sum from the checksum online: fbd2855a20c3610050ff2496dd975821  dmel-all-chromosome-r6.36.fasta.gz 
+#manual check, they match! 
+
+
 
 
