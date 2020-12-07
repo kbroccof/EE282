ef89663c4642f7163205071a497e4924a1d0d217
kbroccof <kbroccof@uci.edu>
2020-12-07 15:15:38 -0800

    Genome Assembly

---
+++ ScriptsHW4/Assemble.sh
@@ -0,0 +1,30 @@
+#### Install minimap and miniasm: 
+conda install -y miniasm minimap 
+
+##### Using interactive shell: 
+srun -c 32 -A ecoevo282 --pty --x11 bash -i
+
+
+#### Using minimap to overlap reads: 
+gunzip iso1_onp_a2_1kb.fastq.gz
+less iso1_onp_a2_1kb.fastq
+q 
+
+minimap -t 32 -Sw5 -L100 -m0 iso1_onp_a2_1kb.fastq{,} \
+> $minimap_check.paf
+less $minimap_check.paf
+q
+
+minimap -t 32 -Sw5 -L100 -m0 iso1_onp_a2_1kb.fastq{,} \
+> $minimap_overlap.paf
+less $minimap_overlap.paf
+q 
+
+
+#### Using miniasm to construct an assembly:
+miniasm $minimap_overlap.paf \
+> $miniasm_assembly.gfa
+less $miniasm_assembly.gfa
+q
+ 
+
