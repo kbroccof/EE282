b871d26745af9d426024943d2908d1666472c01c
kbroccof <kbroccof@uci.edu>
2020-12-09 11:59:59 -0800

    Assembly Assessment

--- ScriptsHW4/AssemblyAssessment.sh
+++ ScriptsHW4/AssemblyAssessment.sh
@@ -5,17 +5,70 @@ conda install -c bioconda -c conda-forge busco=4.1.4
 miniasm $minimap_overlap.paf \
 > $miniasm_assembly.gfa
 
+#####Calculate the length of the sequences of the assembly: 
+faSize $miniasm_assembly.gfa \
+> $miniasm_assembly_size.gfa
+less $miniasm_assembly_size.gfa
+q
+#output: 
+0 bases (0 N's 0 real 0 upper 0 lower) in 0 sequences in 1 files
+%100.00 masked total, %100.00 masked real
+
+MAYBE: worked but it's empty ..... 
+n50 () {
+  bioawk -c fastx ' { print length($seq); n=n+length($seq); } END { print n; } ' $1 \
+  | sort -rn \
+  | gawk ' NR == 1 { n = $1 }; NR > 1 { ni = $1 + ni; } ni/n > 0.5 { print $1; exit; } '
+} 
+
 miniasm $minimap_overlap.paf \
-| 
-> $miniasm_assembly_N50.gfa
+> $miniasm_assembly.gfa \
+| awk ' $0 ~/^S/ { print ">" $2" \n" $3 } ' $miniasm_assembly.gfa \
+| tee >(n50 /dev/stdin > n50.txt) \
+| fold -w 60 \
+> $miniasm_assembly_N50processed.fa
+less $miniasm_assembly_N50processed.fa
+q
+
+
+
+
+
+JJ's script from Nov 30th: first one that is greater than 50% is the N50
+miniasm $minimap_overlap.paf \ 
+| gawk ' { tot=tot+$1; print $1 "\t" tot } END { print tot } ' \
+| sort -k1,1rn \
+| gawk ' NR == 1 { tot = $1 } NR >1 && $2/tot >= 0.5  { print $1 } '
+#output
+gawk: cmd. line:1: (FILENAME=- FNR=2) fatal: division by zero attempted
+
+miniasm $minimap_overlap.paf \ 
+| gawk ' { tot=tot+$1; print $1 "\t" tot } END { print tot } ' \
+| sort -k1,1rn \
+| gawk ' NR == 1 { tot = $1 } NR == 1 { tot = $1 } NR >1 { print $0 "\t" $2 / tot } ' | column t
+
+JJ's script from Nov 30th: first one that is greater than 50% is the N50
+miniasm $minimap_overlap.paf \ 
+| gawk ' { tot=tot+$1; print $1 "\t" tot } END { print tot } ' \
+miniasm_assembly.gfa \
+| sort -k1,1rn \
+| gawk ' NR == 1 { tot = $1 } NR >1 && $2/tot >= 0.5  { print $1 } ' 
+
+
+| column -t \
+> $miniasm_assembly_N50.fa
+less miniasm_assembly_N50.fa
+q
+
 
 #script adapted from Paula's: 
 awk ' $0 ~/^S/ { print ">" $2" \n" $3 } ' $miniasm_assembly.gfa \
 | tee >(n50 /dev/stdin > n50.txt) \
 | fold -w 60 \
 > $miniasm_assembly_N50.fa
+#ouput: bash: n50: command not found...
 
-awk ' $0 ~/^S/ { print ">" $2" \n" $3 } ' processed.gfa \
-| tee >(n50 /dev/stdin > n50.txt) \
-| fold -w 60 \
-> processed.fa
+
+
+
+
