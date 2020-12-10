a486393300fe0dfd712f3b49a2cb67acbe764e72
kbroccof <kbroccof@uci.edu>
2020-12-10 09:43:37 -0800

    Assembly Assessment

--- ScriptsHW4/AssemblyAssessment.sh
+++ ScriptsHW4/AssemblyAssessment.sh
@@ -1,19 +1,14 @@
 #### Install busco: 
 conda install -c bioconda -c conda-forge busco=4.1.4 
 
-#### Calculate N50 of the assembly: MAYBE works?  
-miniasm $minimap_overlap.paf \
-> $miniasm_assembly.gfa
-less $miniasm_assembly.gfa
-q
- 
+#### Calculate N50 of the assembly:
 n50 () {
   bioawk -c fastx ' { print length($seq); n=n+length($seq); } END { print n; } ' $1 \
   | sort -rn \
   | gawk ' NR == 1 { n = $1 }; NR > 1 { ni = $1 + ni; } ni/n > 0.5 { print $1; exit; } '
 } 
 
-miniasm $minimap_overlap.paf \
+miniasm -f iso1_onp_a2_1kb.fastq $minimap_overlap.paf \
 > $miniasm_assembly.gfa \
 | awk ' $0 ~/^S/ { print ">" $2" \n" $3 } ' $miniasm_assembly.gfa \
 | tee >(n50 /dev/stdin > n50.txt) \
